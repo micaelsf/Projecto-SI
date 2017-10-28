f79eb94e40cfdc9a64acb8bbbf12c64f78a4d197
Micael Ferreira <micaelsf@sapo.pt>
2017-10-28 17:26:36 +0100

    Refactored entire project

---
+++ AirMonit_Project/AirMonit_DataHandler/AirMonit_DU/DataUpload.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirMonit_DU
+{
+    public class DataUpload
+    {
+        AirSensorNodeDll.AirSensorNodeDll dll;
+        private DataSensor dataLocation_;
+        private int currentIteration_;
+        private Boolean initialized = false;
+        
+        public DataSensor DataLocation
+        {
+            get
+            {
+                return dataLocation_;
+            }
+        }
+
+        public int Iteration
+        {
+            get { return currentIteration_; }
+        }
+
+        public DataUpload()
+        {
+            dataLocation_ = new DataSensor(0, 0, 0, "", "");
+        }
+
+        public Boolean InitializeSensor()
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                dll = new AirSensorNodeDll.AirSensorNodeDll();
+                dll.Initialize(TransformData, 500);
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean StopSensor()
+        {
+            if (initialized)
+            {
+                initialized = false;
+                dll.Stop();
+                return true;
+            }
+            return false;
+        }
+
+        private void TransformData(string message)
+        {
+            string[] splited_val = message.Split(';');
+
+            if (splited_val[1] == "CO")
+            {
+                dataLocation_.CO_data = Double.Parse(splited_val[2]);
+            }
+            if (splited_val[1] == "NO2")
+            {
+                dataLocation_.NO2_data = Double.Parse(splited_val[2]);
+            }
+            if (splited_val[1] == "O3")
+            {
+                dataLocation_.O3_data = Double.Parse(splited_val[2]);
+            }
+                
+            dataLocation_.City = splited_val[4];
+
+            dataLocation_.Date = splited_val[3];
+
+            currentIteration_ = int.Parse(splited_val[0]);
+
+        }
+    }
+}
---
+++ AirMonit_Project/AirMonit_DataHandler/CommunicationChannelInfrastructure/Program.cs
@@ -0,0 +1,101 @@
+﻿using AirMonit_DU;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CommunicationChannelInfrastructure
+{
+    class Program
+    {
+        //private static string xmlPath = AppDomain.CurrentDomain.BaseDirectory + @"data/air_data.xml";
+        private static string xmlPath = @"air_data.xml";
+        private static XmlDocument doc;
+        private static XmlElement root;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("..::Communication channel Initialized::..");
+            Console.WriteLine("\n");
+
+            DataUpload du = new DataUpload();
+            DataSensor data;
+            Boolean initializedSensors;
+
+            CreateBaseXMLStructure();
+            initializedSensors = du.InitializeSensor();
+
+            while (initializedSensors)
+            {
+                data = CallDataSensors(du);
+                if (data != null)
+                {
+                    Console.WriteLine(data.toString());
+                    //handleData(value);
+                    Console.WriteLine("\n");
+
+                }
+            }
+        }
+
+        private static DataSensor CallDataSensors(DataUpload du)
+        {
+            int iteration = 0;
+            Thread.Sleep(500);
+
+            iteration = du.Iteration;
+
+            if (iteration % 3 == 0)
+            {
+                return du.DataLocation;
+            }
+
+            return null;
+        }
+
+        private static void HandleData(DataSensor value)
+        {
+            root.AppendChild(CreateData(value));
+            doc.Save(xmlPath);
+        }
+
+        private static XmlNode CreateData(DataSensor value)
+        {
+            XmlElement data = doc.CreateElement("air_data");
+
+            XmlElement city = doc.CreateElement("city");
+            XmlElement co = doc.CreateElement("co");
+            XmlElement no2 = doc.CreateElement("no2");
+            XmlElement o3 = doc.CreateElement("o3");
+            XmlElement date = doc.CreateElement("date");
+
+            data.AppendChild(city);
+            city.InnerText = value.City;
+            data.AppendChild(co);
+            co.InnerText = value.CO_data.ToString();
+            data.AppendChild(no2);
+            no2.InnerText = value.NO2_data.ToString();
+            data.AppendChild(o3);
+            o3.InnerText = value.O3_data.ToString();
+            data.AppendChild(date);
+            date.InnerText = value.Date;
+
+            return data;
+        }
+
+        private static void CreateBaseXMLStructure()
+        {
+            doc = new XmlDocument();
+            XmlDeclaration decl = doc.CreateXmlDeclaration("1.0", null, null);
+            doc.AppendChild(decl);
+
+            root = doc.CreateElement("air_quality_data");
+            doc.AppendChild(root);
+
+            doc.Save(xmlPath);
+        }
+    }
+}
