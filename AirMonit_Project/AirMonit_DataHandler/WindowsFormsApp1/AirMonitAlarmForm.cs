baa06f5f7bbb69882bedd6de5bd5f05d08425fa3
Micael Ferreira <micaelsf@sapo.pt>
2017-10-28 21:33:36 +0100

    AirMonit_Alarm started (had to test some features), there are some protections to be done at input 'frequency'

---
+++ AirMonit_Project/AirMonit_DataHandler/WindowsFormsApp1/AirMonitAlarmForm.cs
@@ -0,0 +1,78 @@
+﻿using AirMonit_DU;
+using CommunicationChannelInfrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class AirMonitAlarm : Form
+    {
+        private CCI cci;
+        // time by default is 500ms
+        private static int TIME = 500;
+        private Boolean initializedSensors;
+
+        public AirMonitAlarm()
+        {
+            InitializeComponent();
+            cci = new CCI();
+        }
+
+        private void InitSensorsButton_Click(object sender, EventArgs e)
+        {
+            // To avoid freezing the UI, we create a thread to handle the listbox populate
+            Thread thread1 = new Thread(populateList);
+            thread1.Start();
+        }
+
+        private void populateList()
+        {
+            DataSensor data;
+            cci = new CCI();
+
+            try
+            {
+                int timeTmp = int.Parse(timeTextbox.Text);
+
+                if (timeTmp > 0)
+                {
+                    TIME = (timeTmp * 1000) / 3;
+                }
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            initializedSensors = cci.InitializeSensors(TIME);
+
+            while (initializedSensors)
+            {
+                data = cci.TimeDataSensors(TIME);
+
+                if (data != null)
+                {
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        // insert at first line
+                        listBox1.Items.Insert(0, data.toString());
+                    }));
+
+                }
+            }
+        }
+
+        private void StopSensorsButton_Click(object sender, EventArgs e)
+        {
+            initializedSensors = !cci.StopSensors();
+        }
+    }
+}
