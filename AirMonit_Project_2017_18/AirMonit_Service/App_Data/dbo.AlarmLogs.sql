aa5bd5fabff55dc6bede5d67b3ac837b8d06d273
Bruno Alves <bgualter@gmail.com>
2017-11-23 23:16:21 +0000

    Inicial commit app harbor

--- AirMonit_Project_2017_18/AirMonit_Service/App_Data/dbo.AlarmLogs.sql
+++ AirMonit_Project_2017_18/AirMonit_Service/App_Data/dbo.AlarmLogs.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[AlarmLogs] (
     [Id]            INT           IDENTITY (1, 1) NOT NULL,
     [Description]   NVARCHAR (200) NOT NULL,
-    [Date_Time]     DATETIME      NOT NULL,
+    [Date_Time]     DATETIMEOFFSET      NOT NULL,
     [SensorDataId] INT NOT NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
-
