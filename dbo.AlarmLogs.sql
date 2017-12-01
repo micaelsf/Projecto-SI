eb493ac9fb861faaa5e2156c0609a05e89e3665a
micaelsf <micaelsf@sapo.pt>
2017-12-01 12:42:46 +0000

    fix airMonit_DU when closing program without stop sensors

--- dbo.AlarmLogs.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[AlarmLogs] (
-    [Id]            INT           IDENTITY (1, 1) NOT NULL,
-    [Description]   NVARCHAR (200) NOT NULL,
-    [Date_Time]     DATETIME      NOT NULL,
-    [SensorDataId] INT NOT NULL, 
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
