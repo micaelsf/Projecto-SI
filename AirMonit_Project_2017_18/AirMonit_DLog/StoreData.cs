8cb1ccccfe4e55e0eba27f55f5189eb35b14ff1f
micaelsf <micaelsf@sapo.pt>
2017-12-08 18:12:43 +0000

    Code refactoring and comments added

--- AirMonit_Project_2017_18/AirMonit_DLog/StoreData.cs
+++ AirMonit_Project_2017_18/AirMonit_DLog/StoreData.cs
@@ -6,9 +6,12 @@ namespace AirMonit_DLog
 {
     public class StoreData
     {
-        //private string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["appHarborConnect"].ConnectionString;
         //private SqlConnection connection;
+
+        // appHarbor dataset
         private AppHarborDataSet appHarborDataSet;
+
+        // appHabror adapters, needed to update the tables at server, or get data from it
         private AppHarborDataSetTableAdapters.SensorDataTableAdapter sensorDataTableAdapter;
         private AppHarborDataSetTableAdapters.AlarmLogsTableAdapter alarmDataTableAdapter;
         private AppHarborDataSetTableAdapters.CitiesTableAdapter citiesTableAdapter;
@@ -16,6 +19,7 @@ namespace AirMonit_DLog
         public StoreData()
         {
             //connection = new SqlConnection(connectionString);
+
             appHarborDataSet = new AppHarborDataSet();
             sensorDataTableAdapter = new AppHarborDataSetTableAdapters.SensorDataTableAdapter();
             alarmDataTableAdapter = new AppHarborDataSetTableAdapters.AlarmLogsTableAdapter();
@@ -25,18 +29,18 @@ namespace AirMonit_DLog
         public void StoreSensorData(SensorData sensorData)
         {
             AppHarborDataSet.SensorDataRow newSensorDataRow;
+            newSensorDataRow = (AppHarborDataSet.SensorDataRow)appHarborDataSet.SensorData.NewRow();
 
             int cityId = GetCityIdFrom(sensorData.City);
 
             try { 
-                newSensorDataRow = (AppHarborDataSet.SensorDataRow) appHarborDataSet.SensorData.NewRow();
                 newSensorDataRow.Param = sensorData.Param;
                 newSensorDataRow.Value = sensorData.Value;
                 newSensorDataRow.CityId = cityId;
                 newSensorDataRow.DateTime = DateTime.Parse(sensorData.Date + " " + sensorData.Time);
                 newSensorDataRow.SensorId = sensorData.Id;
 
-                // Add the row to the Region table
+                // Add the row to the SensorData table
                 this.appHarborDataSet.SensorData.Rows.Add(newSensorDataRow);
 
                 // Save the new row to the database
@@ -51,15 +55,15 @@ namespace AirMonit_DLog
         public void StoreAlarmData(AlarmData alarmData)
         {
             AppHarborDataSet.AlarmLogsRow newAlarmDataRow;
+            newAlarmDataRow = (AppHarborDataSet.AlarmLogsRow)appHarborDataSet.AlarmLogs.NewRow();
 
             try
             {
-                newAlarmDataRow = (AppHarborDataSet.AlarmLogsRow)appHarborDataSet.AlarmLogs.NewRow();
                 newAlarmDataRow.Description = alarmData.AlarmDescription;
                 newAlarmDataRow.Date_Time = DateTime.Parse(alarmData.AlarmDate + " " + alarmData.AlarmTime);
                 newAlarmDataRow.SensorDataId = int.Parse(alarmData.SensorId);
 
-                // Add the row to the Region table
+                // Add the row to the AlarmLogs table
                 this.appHarborDataSet.AlarmLogs.Rows.Add(newAlarmDataRow);
 
                 // Save the new row to the database
@@ -77,9 +81,11 @@ namespace AirMonit_DLog
             DataRow[] dataRows;
 
             int cityId;
+
+            // get all data from Cities table
             dataTable = this.citiesTableAdapter.GetData();
 
-            dataRows = dataTable.Select("City_Name = '" + cityName +"'");
+            dataRows = dataTable.Select("City_Name = '" + cityName + "'");
             
             cityId = Convert.ToInt32(dataRows[0]["Id"]);
 
